dd63768c6bea5ea5447f4353adf8b36f25169ba6
Howard Stark <howard@getcoffee.io>
2018-01-15 12:18:24 -0800

    :b:o:b::b:y :b:

--- objecttracking/cubetrack.cpp
+++ objecttracking/cubetrack.cpp
@@ -4,24 +4,6 @@
 #include <opencv2/core.hpp>
 
 namespace ObjectTracking {
-    Cube::Cube(Pose pose) : pose(pose) {};
-
-    /*
-     * I did this not because I should
-     * but because I knew I could
-     */
-    std::vector<Cube> Cube::findCubes() {
-        std::vector<Cube> cubes;
-        std::transform(Field::getInstance()->objects.begin(), Field::getInstance()->objects.end(),
-                       cubes.begin(),
-                       [](Pose p)->Cube{
-                           return Cube(p);
-                       }
-        );
-        return cubes;
-    }
-
     void tick() {
-        Cube::findCubes();
     }
 }
--- objecttracking/cubetrack.hpp
+++ objecttracking/cubetrack.hpp
@@ -5,12 +5,6 @@
 #include "../objects.hpp"
 
 namespace ObjectTracking {
-    class Cube {
-    public:
-        Cube(Pose pose);
-        Pose pose;
-        static std::vector<Cube> findCubes();
-    };
     void tick();
 }
 
