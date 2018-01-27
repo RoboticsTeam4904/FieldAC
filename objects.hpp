c0e14d99cd1b616e6b4d47a8efa4f326ac07b7d8
Leijurv <leijurv@gmail.com>
2018-01-27 14:12:22 -0800

    kill your life

--- objects.hpp
+++ objects.hpp
@@ -5,11 +5,12 @@
 
 struct Pose {
     Pose();
-    Pose(const Pose prev, const float measuredAccelForward, const float measuredAccelYaw);
+    Pose(const Pose prev, const float measuredAccelForward, const float measuredAccelLateral, const float measuredAccelYaw);
     float x;
     float y;
+    float dx;
+    float dy;
     float yaw;
-    float rateForward;
     float rateYaw;
     float plausibility(const std::string sensorData);
     void seed();
