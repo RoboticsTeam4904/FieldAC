f6a4d72ae5b9cc3635a2b47fbbd710a9e11457a3
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2018-02-19 15:46:14 -0800

    Check if it's empty

--- objecttracking/cubetrack.cpp
+++ objecttracking/cubetrack.cpp
@@ -85,6 +85,9 @@ namespace ObjectTracking {
                 }
             }
             std::printf("Size of opticalFlowBox is %d", opticalFlowBox.size());
+            if (!opticalFlowBox.size()) {
+                continue;
+            }
             this->tracker_flow->update_cur_bbox_vec(opticalFlowBox);
             opticalFlowBox = this->tracker_flow->tracking_flow(frame, true);
             optflowFrame = frame.clone();
