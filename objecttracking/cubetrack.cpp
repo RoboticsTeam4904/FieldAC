79bdeb6d954b745b85bda94ae7d4ef50b46c8e81
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2018-02-20 16:34:05 -0800

    it works!

--- objecttracking/cubetrack.cpp
+++ objecttracking/cubetrack.cpp
@@ -26,7 +26,7 @@ namespace ObjectTracking {
 
     void CubeTracker::update(cv::Mat frameUpdate) {
         this->mutexFrame.lock();
-        this->lastLastFrame = this->lastFrame.clone();
+        this->lastFrame.copyTo(this->lastLastFrame);
         this->lastFrame = frameUpdate;
         this->mutexFrame.unlock();
         this->track_optflow_queue.push(frameUpdate);
@@ -35,7 +35,6 @@ namespace ObjectTracking {
     void CubeTracker::run() {
         std::printf("Running CubeTracker in here!\n");
         cv::Mat frame;
-        float cur_time_extrapolate = 0, old_time_extrapolate = 0;
         std::vector<bbox_t> opticalFlowBox;
         while(true) {
             this->mutexFrame.lock();
@@ -45,9 +44,9 @@ namespace ObjectTracking {
             if (frame.empty()) {
                 continue;
             }
-            std::vector<cv::Point> features_prev, features_next;
+            std::vector<cv::Point2f> features_prev, features_next;
 
-            int max_count = 100;
+            int max_count = 1000;
             std::printf("suck\n");
             cv::Mat gray(this->lastFrame.size(), CV_8UC1);
             cv::cvtColor(this->track_optflow_queue.front(), gray, CV_BGR2GRAY, 1);
@@ -57,39 +56,52 @@ namespace ObjectTracking {
                                     0.01,     // quality level
                                     10
             );
-            std::printf("size of thingyyyyyyy: %d\n\n", features_next.size());
+            std::printf("FOUND N E W  POINTS         - - - - - \n");
             for (auto &i : features_next) {
-                std::printf(" - feature: (%d, %d)\n", i.x, i.y);
+                std::printf(" - feature: (%lf, %lf)\n", i.x, i.y);
             }
             while (this->track_optflow_queue.size() > 1) {
-                std::printf("i want to die please and thank you\n");
+                std::printf("i want to die please and thank you, size is %d\n", this->track_optflow_queue.size());
                 cv::Mat current_frame(this->lastFrame.size(), CV_8UC1); // Initialize greyscale current frame mat
                 cv::cvtColor(this->track_optflow_queue.front(), current_frame, CV_BGR2GRAY, 1); // Convert front of queue to greyscale and put it in current_frame
-
                 this->track_optflow_queue.pop();
 
                 cv::Mat next_frame(this->lastFrame.size(), CV_8UC1);
                 cv::cvtColor(this->track_optflow_queue.front(), current_frame, CV_BGR2GRAY, 1); // Convert front of queue to greyscale and put it in next_frame
+                this->track_optflow_queue.pop();
 
                 features_prev = features_next;
-                cv::Mat status;
-                std::vector<cv::Point> err;
-                std::printf("size of 1: %lu\nsize of 2:%lu\n", features_next.size(), features_prev.size());
+                std::vector<unsigned char> status;
+                std::vector<float> err;
+                std::printf("size of 1: %lu\nsize of 2: %lu\n", features_next.size(), features_prev.size());
+
+                std::printf("are they equal? %d\n", cv::countNonZero(current_frame != next_frame) == 0);
                 cv::calcOpticalFlowPyrLK(
                         current_frame, next_frame, // 2 consecutive images
                         features_prev, // input point positions in first im
                         features_next, // output point positions in the 2nd
                         status,    // tracking success
-                        err      // tracking error
+                        err     // tracking error
                 );
-                std::printf("Did optical flow\n");
+
+                std::printf("\n\nDid optical flow ------------------\n");
+                bool a = false;
+                optflowFrame = current_frame.clone();
                 for (int i = 0; i < features_next.size(); i++) {
-                    std::printf(" --- point (%d, %d) -> (%d, %d)", features_prev[i].x, features_prev[i].y, features_next[i].x, features_next[i].y);
+                    if (features_next[i] != features_prev[i]) {
+                        cv::line(optflowFrame, features_prev[i], features_next[i], cv::Scalar( 0, 255, 0 ), 1);
+                        std::printf(" --- point (%lf, %lf) -> (%lf, %lf)\n", features_prev[i].x, features_prev[i].y, features_next[i].x, features_next[i].y);
+                        a = true;
+                    }
                 }
+                if (a) {
+                    std::printf("THEYRE ALL NOT EQUAL REEE");
+                }
+
+
+                this->draw_boxes(optflowFrame, opticalFlowBox);
             }
 
-            optflowFrame = frame.clone();
-            this->draw_boxes(optflowFrame, opticalFlowBox);
         }
 
 
