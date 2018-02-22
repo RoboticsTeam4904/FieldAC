25c2ff5335e558a423f90e00535e99247c747ba7
Howard Stark <howard@getcoffee.io>
2018-02-21 17:05:25 -0800

    Synchronized mutex breaks it?????

---
+++ objecttracking/sharedqueue.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Howard Stark on 2/21/18.
+//
+
+#ifndef INC_2018_FIELD_SHAREDQUEUE_HPP
+#define INC_2018_FIELD_SHAREDQUEUE_HPP
+
+
+
+class sharedqueue {
+
+};
+
+
+
+#endif //INC_2018_FIELD_SHAREDQUEUE_HPP
