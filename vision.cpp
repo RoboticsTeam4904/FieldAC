85ed21bc91b7e6e3235c4f1d65621f822de102db
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2018-03-18 11:51:51 -0700

    fixes

--- vision.cpp
+++ vision.cpp
@@ -53,7 +53,7 @@ namespace Vision {
             }
             if(src) {
                 // emulate camera delay
-                std::this_thread::sleep_for(std::chrono::milliseconds(80));
+                std::this_thread::sleep_for(std::chrono::milliseconds(110));
             }
         }
     }
