3cab9ea708d73af17186d07eb14ad89c6c0ccaa8
Howard Stark <howard@getcoffee.io>
2018-03-20 05:55:12 -0700

    Sudo's back

--- scripts/install_cmake-3.9.sh
+++ scripts/install_cmake-3.9.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 curl -sL https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.sh > cmake-3.9.1.sh
-sh cmake-3.9.1.sh --prefix=/usr/local/ --exclude-subdir
+sudo sh cmake-3.9.1.sh --prefix=/usr/local/ --exclude-subdir
--- scripts/install_opencv-3.4.1.sh
+++ scripts/install_opencv-3.4.1.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
-apt-get install -y build-essential
-apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
 curl -sL https://github.com/opencv/opencv/archive/3.4.1.zip > opencv.zip
 unzip opencv.zip
@@ -14,8 +14,8 @@ cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make -j4
 
-make install
-sh -c 'echo "/usr/local/lib" > etc/ld.so.conf.d/opencv.conf'
-ldconfig
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" > etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
 
 echo "OpenCV installed."
