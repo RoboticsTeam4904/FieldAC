6d00d4b17e351fa48b1fcd8f73684cef1fbf9fb6
Howard Stark <howard@getcoffee.io>
2018-03-20 05:37:03 -0700

    no preserve root

--- scripts/install_cmake-3.9.sh
+++ scripts/install_cmake-3.9.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 curl -sL https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.sh > cmake-3.9.1.sh
-sudo sh cmake-3.9.1.sh --prefix=/usr/local/ --exclude-subdir
+sh cmake-3.9.1.sh --prefix=/usr/local/ --exclude-subdir
